783cb798d25d6eb994e70bfa7dadc27091d698b3
LuckyXII <omnipotent117@gmail.com>
2017-09-28 22:59:28 +0200

    modified database

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,12 @@
 //=======================================================
+//IMPORTS
+const car = require("../models/carModel");
+
+//=======================================================
 //GLOBALS
 const searchBtn = document.getElementById("searchBtn");
-
+const dateStart = document.getElementById("dateForm").children[0];
+const dateEnd = document.getElementById("dateForm").children[1];
 //=======================================================
 //CLASSES
 
@@ -9,11 +14,23 @@ const searchBtn = document.getElementById("searchBtn");
 //MAIN
 
 //=======================================================
-//CALLBACKS
-
+//LISTENERS
+searchBtn.addEventListener("click",checkAvailabillityByQuery);
+dateStart.addEventListener("change",checkAvailabillityByDate);
+dateStart.addEventListener("change",checkAvailabillityByDate);
 //=======================================================
 //FUNCTIONS
 
-function filterByQuery(){
+function checkAvailabillityByQuery(e){
+    e.preventDefault();
+    const dateStartValue = dateStart.valueAsDate;
+    const dateEndValue = dateEnd.valueAsDate;
+    //TODO add rest of input data, make query and filter cars
+
+}
+
+function checkAvailabillityByDate(e){
+    console.log(e);
+    
 
 }
