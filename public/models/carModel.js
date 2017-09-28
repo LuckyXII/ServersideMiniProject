1a8aef82510a72ea89a0ce254e9c38312ebebb1a
LuckyXII <omnipotent117@gmail.com>
2017-09-28 11:46:02 +0200

    add carScheme, remodel structure, fix misstake

---
+++ public/models/carModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+    reqLicense: String,
+    fordonstyp:String,
+    brand:{
+        type:String,
+        required:true
+    },
+    model:{
+        type:String,
+        required:true
+    },
+    year: Number,
+    gearbox:String,
+    dagsHyra: Number,
+    fuel: String,
+    imgLink: String,
+    kommentarer: String,
+    isAvailable: Boolean
+});
+
+module.exports = mongoose.model("vehicle", carSchema);
