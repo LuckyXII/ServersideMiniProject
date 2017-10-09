138993c790372c820dc92f4d9b47ea9952fdc524
matijastampfel <matija.stampfel@gmail.com>
2017-10-09 18:01:00 +0200

    id's and admin.js (start)

---
+++ public/scripts/admin.js
@@ -0,0 +1,17 @@
+//=======================================================
+//GLOBALS
+const
+    searchB = document.getElementById("searchB"),
+    selectFuel = document.getElementById("fuel"),
+    selectLicence = document.getElementById("licence"),
+    selectStatus = document.getElementById("isAvailable"),
+    selectComment = document.getElementById("comment"),
+    selectGearbox = document.getElementById("gearbox"),
+    selectBrand = document.getElementById("brand"),
+    selectModel = document.getElementById("model"),
+    selectVehicleType = document.getElementById("vehicleType"),
+    selectDate = document.getElementById("date"),
+    selectPrice = document.getElementById("price")
+    addB = document.getElementById("addB"),
+    deleteB = document.getElementById("deleteB"),
+    updateB = document.getElementById("updateB"),
